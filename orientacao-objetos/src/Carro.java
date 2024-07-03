f7ff42c65e4557f783d36e3678d6741d107c58e2
Rebeca Cagni <160529030+RebecaCagni@users.noreply.github.com>
2024-07-02 22:45:37 -0300

    Orientacao-Objetos
    Nesse projeto foi criado a primeira aplicação utilizando o paradigma de orientação a objetos em java

---
+++ orientacao-objetos/src/Carro.java
@@ -0,0 +1,11 @@
+public class Carro {
+
+    public String cor;
+
+    public  String marca;
+
+    public  String modelo;
+
+    public  int ano;
+
+}
---
+++ orientacao-objetos/src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+    Carro fusca = new Carro();
+    fusca.modelo = "fusca";
+    fusca.marca = "VolksWagem";
+    fusca.cor = "Preto";
+    fusca.ano = 1978;
+
+        System.out.println(fusca.ano);
+    }
+}

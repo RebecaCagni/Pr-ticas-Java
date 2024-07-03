ad87a0b35ad0694fde3d61c384f920290b3f4f88
Rebeca Cagni <160529030+RebecaCagni@users.noreply.github.com>
2024-07-02 22:47:18 -0300

    Projeto Primeiros contatos com Java
    Nesse projeto foi realizado estruturas de if, loops, noção de tipos de dados e variáveis...

---
+++ PrimeiroProjetoJava/src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+    int[] arraysimples = {1,2,6,8};
+
+    for (var index = 0; index < arraysimples.length; index++) {
+        System.out.println(arraysimples[index]);
+    }
+
+//        System.out.println(arraysimples.length);
+    }
+}
